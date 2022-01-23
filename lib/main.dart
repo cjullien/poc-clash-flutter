fac4930c92d9f902e65aabf0056feef487105a5b
FlutLab.io <>
2022-01-23 13:16:49 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,9 @@ class _MyAppState extends State<MyApp> {
   void getData() {
     futureDynamic
         .then((resultList) => setState(() {
+              persons.add(Person(name: "test"));
               persons.add(Person(name: "$resultList"));
-              persons.addAll(resultList.map((Map<String, dynamic> p) {
+              persons.addAll(resultList.foreach((Map<String, dynamic> p) {
                 return Person.fromJson(p);
               }).toList());
             }))
