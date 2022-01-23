3142bd730b8513358f4093bb67c5351e50ca5fce
FlutLab.io <>
2022-01-23 16:30:05 +0000

    Autosave

--- lib/MemoryImageWidget.dart
+++ lib/MemoryImageWidget.dart
@@ -1,12 +1,11 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_image_widget/image_string.dart';
 
 class MemoryImageWidget extends StatelessWidget {
 
-  String base64Image;
+  final String base64Image;
 
-  MemoryImageWidget(base64Image="")
+  const MemoryImageWidget({this.base64Image=""})
 
   @override
   Widget build(BuildContext context) {
