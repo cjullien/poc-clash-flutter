f84ad30a07454fd47c4894b331d47d96904199cf
FlutLab.io <>
2022-01-24 23:17:36 +0000

    Autosave

--- lib/models/Person.dart
+++ lib/models/Person.dart
@@ -1,5 +1,12 @@
 import 'package:json_serializable/json_serializable.dart';
 
+/// This allows the `User` class to access private members in
+/// the generated file. The value for this is *.g.dart, where
+/// the star denotes the source file name.
+part 'user.g.dart';
+
+/// An annotation for the code generator to know that this class needs the
+/// JSON serialization logic to be generated.
 @JsonSerializable()
 class Person {
   final String firstname;
