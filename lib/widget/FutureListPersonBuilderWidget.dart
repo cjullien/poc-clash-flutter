3d84d73c682b836bf23038f18a0f241968392af3
FlutLab.io <>
2022-01-24 21:06:39 +0000

    Autosave

--- lib/widget/FutureListPersonBuilderWidget.dart
+++ lib/widget/FutureListPersonBuilderWidget.dart
@@ -5,13 +5,33 @@ import 'package:flutter/material.dart';
 import 'package:adress_book/models/Person.dart';
 import 'package:adress_book/utils/FuturGeneric.dart';
 import 'package:adress_book/widget/ContactCard.dart';
+import 'package:adress_book/main.dart';
 
-class FutureListPersonBuilderWidget extends StatelessWidget {
-  late final Future<dynamic> futureDynamic;
+class FutureListPersonBuilderWidget extends StatefulWidget {
   final List<Person> persons;
+
+  late Future<dynamic> futureDynamic = Future<dynamic>.delayed(
+    const Duration(seconds: 1),
+    () => fetchDynamic(),
+  );
+
+  State createState() {}
 
   FutureListPersonBuilderWidget(this.persons) {
     futureDynamic = fetchDynamic();
+    getData();
+  }
+
+  void getData() {
+    futureDynamic
+        .then((resultList) => setState(() {
+              persons.add(Person(name: "test2"));
+              persons.addAll(List<Person>.from(resultList.map((model) => Person.fromJson(model))));
+            }))
+        .catchError((error) {
+      //errorMsg = error.toString();
+      throw error;
+    });
   }
 
   Widget build(BuildContext context) {
