82e44aa3db5a135efc4ae588b82070c817ac74c0
FlutLab.io <>
2022-01-24 23:07:41 +0000

    Autosave

--- lib/models/Person.dart
+++ lib/models/Person.dart
@@ -8,8 +8,13 @@ class Person {
   const Person({this.firstname = "", this.name = "", this.email = "", this.phone = "", this.picture = ""});
 
   factory Person.fromJson(Map<String, dynamic> json) {
-    return Person(firstname: json['username'], name: json['name'], email: json['email'], phone: json['phone'], picture: "" //""['picture'],
-        );
+    return Person(
+      firstname: json['username'],
+      name: json['name'],
+      email: json['email'],
+      phone: json['phone'],
+      picture: json['picture'] == null ? null : json['picture'],
+    );
   }
 
   /*toString() {
