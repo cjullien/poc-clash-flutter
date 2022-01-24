9ee6a0ee212bc5e4f53f334eb6a66bd753df603f
FlutLab.io <>
2022-01-24 23:04:34 +0000

    Autosave

--- lib/widget/ContactCard.dart
+++ lib/widget/ContactCard.dart
@@ -16,20 +16,20 @@ class ContactCard extends StatelessWidget {
         color: Colors.white,
         child: Row(
           children: [
+            Center(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Expanded(
+                  child: MemoryImageWidget(person.picture),
+                  flex: 2,
+                ),
+              ),
+            ),
             Expanded(
               child: Container(
                 alignment: Alignment.topLeft,
                 child: Column(
                   children: [
-                    Center(
-                      child: Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Expanded(
-                          child: MemoryImageWidget(person.picture),
-                          flex: 2,
-                        ),
-                      ),
-                    ),
                     Expanded(
                       flex: 5,
                       child: ListTile(
