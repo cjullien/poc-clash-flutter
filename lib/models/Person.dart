4c8e8eb672867a6f2a10ffa267a13f9639a4746d
FlutLab.io <>
2022-01-24 23:13:35 +0000

    Autosave

--- lib/models/Person.dart
+++ lib/models/Person.dart
@@ -1,3 +1,6 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
 class Person {
   final String firstname;
   final String name;
@@ -7,15 +10,9 @@ class Person {
 
   const Person({this.firstname = "", this.name = "", this.email = "", this.phone = "", this.picture = ""});
 
-  factory Person.fromJson(Map<String, dynamic> json) {
-    return Person(
-      firstname: json['username'],
-      name: json['name'],
-      email: json['email'],
-      phone: json['phone'],
-      picture: json['picture'] == null ? null : json['picture'],
-    );
-  }
+
+    factory Person.fromJson(Map<String, dynamic> json) => _$PersonFromJson(json);
+  Map<String, dynamic> toJson() => _$PersonToJson(this)
 
   /*toString() {
     return "Person { firstname: $firstname , name: $name, email: $email }";
