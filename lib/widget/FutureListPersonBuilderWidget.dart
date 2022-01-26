9adf6cade0b8ef4b01944246425ca7d8a21ada5d
FlutLab.io <>
2022-01-26 20:12:23 +0000

    Autosave

--- lib/widget/FutureListPersonBuilderWidget.dart
+++ lib/widget/FutureListPersonBuilderWidget.dart
@@ -37,7 +37,7 @@ class FutureListPersonBuilderWidget extends StatelessWidget {
                   xs: 12,
                   md: 6,
                   lg: 3,
-                  child: Text("toto"), //ContactCard(p),
+                  child: Text(p), //ContactCard(p),
                 ),
               )
               .toList(),
