60c2569f7f7e0a6c17881729d147097f1a31b6e7
FlutLab.io <>
2022-01-26 21:39:27 +0000

    suppression bootstrap

--- lib/widget/FutureListPersonBuilderWidget.dart
+++ lib/widget/FutureListPersonBuilderWidget.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 import 'package:adress_book/models/Person.dart';
 import 'package:adress_book/widget/ContactCard.dart';
-import 'package:flutter_bootstrap/flutter_bootstrap.dart';
 
 class FutureListPersonBuilderWidget extends StatelessWidget {
   final List<Person> persons;
@@ -17,18 +16,7 @@ class FutureListPersonBuilderWidget extends StatelessWidget {
     return FutureBuilder<dynamic>(
       future: futureDynamic,
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-        return /*BootstrapContainer(
-          fluid: true,
-          children: persons
-              .map(
-                (p) => BootstrapCol(
-                  sizes: 'col-12 col-xs-12 col-sm-6 col-lg-4 col-xl-3',
-                  child: ContactCard(p),
-                ),
-              )
-              .toList(),
-        );*/
-            GridView.count(
+        return GridView.count(
           primary: false,
           padding: const EdgeInsets.all(20),
           crossAxisSpacing: 10,
