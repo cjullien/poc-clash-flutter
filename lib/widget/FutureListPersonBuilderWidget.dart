6eaa5423c78f2306a3e545eca3bd764d2128b823
FlutLab.io <>
2022-01-26 19:22:11 +0000

    Autosave

--- lib/widget/FutureListPersonBuilderWidget.dart
+++ lib/widget/FutureListPersonBuilderWidget.dart
@@ -37,7 +37,7 @@ class FutureListPersonBuilderWidget extends StatelessWidget {
             children: persons
                 .map(
                   (p) => StaggeredGridTile.count(
-                    crossAxisCellCount: (MediaQuery.of(context).size.width ~/ 300) == 0 ? 1 : (MediaQuery.of(context).size.width ~/ 300),
+                    crossAxisCellCount: (MediaQuery.of(context).size.width ~/ 400) == 0 ? 1 : (MediaQuery.of(context).size.width ~/ 300),
                     mainAxisCellCount: 1, //MediaQuery.of(context).size.height as int,
                     child: ContactCard(p),
                   ),
