2a5e9fbd93f6c3c6145811f87fce0b83b642452e
FlutLab.io <>
2022-01-26 21:35:07 +0000

    Autosave

--- lib/widget/FutureListPersonBuilderWidget.dart
+++ lib/widget/FutureListPersonBuilderWidget.dart
@@ -33,8 +33,8 @@ class FutureListPersonBuilderWidget extends StatelessWidget {
           padding: const EdgeInsets.all(20),
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
-          crossAxisCount: (MediaQuery.of(context).size.width ~/ 400),
-          children: persons.map((p) => Container(child: ContactCard(p))).toList(),
+          crossAxisCount: (MediaQuery.of(context).size.width ~/ 300),
+          children: persons.map((p) => ContactCard(p)).toList(),
         );
       },
     );
