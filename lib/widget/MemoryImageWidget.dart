d321c9bc6f588c43db675d68100145d76e64750a
FlutLab.io <>
2022-02-02 21:07:03 +0000

    Autosave

--- lib/widget/MemoryImageWidget.dart
+++ lib/widget/MemoryImageWidget.dart
@@ -11,6 +11,13 @@ class MemoryImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String imageToPrint = image.isNotEmpty ? image : default_base64Image;
-    return MemoryImage(base64Decode(imageToPrint));
+    return Container(
+        height: 200,
+        decoration: BoxDecoration(
+            color: Colors.green,
+            image: Image.memory(
+              base64Decode(imageToPrint),
+              width: 200,
+            )));
   }
 }
