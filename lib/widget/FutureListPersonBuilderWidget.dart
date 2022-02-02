2bff19ec579a3c34406788423feb4a027710b51d
FlutLab.io <>
2022-02-02 19:28:43 +0000

    Autosave

--- lib/widget/FutureListPersonBuilderWidget.dart
+++ lib/widget/FutureListPersonBuilderWidget.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 import 'package:adress_book/models/Person.dart';
 import 'package:adress_book/widget/ContactCard.dart';
-import 'package:flutter_tindercard/flutter_tindercard.dart';
 
 class FutureListPersonBuilderWidget extends StatelessWidget {
   final List<Person> persons;
@@ -17,45 +16,13 @@ class FutureListPersonBuilderWidget extends StatelessWidget {
     return FutureBuilder<dynamic>(
       future: futureDynamic,
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-        CardController controller; //Use this to trigger swap.
-
         return GridView.count(
           primary: false,
           padding: const EdgeInsets.all(20),
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
           crossAxisCount: (MediaQuery.of(context).size.width ~/ 300),
-          children: persons
-              .map(
-                (p) => new TinderSwapCard(
-                  swipeUp: true,
-                  swipeDown: true,
-                  orientation: AmassOrientation.BOTTOM,
-                  totalNum: persons.length,
-                  stackNum: 3,
-                  swipeEdge: 4.0,
-                  maxWidth: MediaQuery.of(context).size.width * 0.9,
-                  maxHeight: MediaQuery.of(context).size.width * 0.9,
-                  minWidth: MediaQuery.of(context).size.width * 0.8,
-                  minHeight: MediaQuery.of(context).size.width * 0.8,
-                  cardBuilder: (context, index) => Card(
-                    child: Image.asset(p.picture), //ContactCard(p),
-                  ),
-                  cardController: controller = CardController(),
-                  swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
-                    /// Get swiping card's alignment
-                    if (align.x < 0) {
-                      //Card is LEFT swiping
-                    } else if (align.x > 0) {
-                      //Card is RIGHT swiping
-                    }
-                  },
-                  swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
-                    /// Get orientation & index of swiped card!
-                  },
-                ),
-              )
-              .toList(),
+          children: persons.map((p) => ContactCard(p)).toList(),
         );
       },
     );
