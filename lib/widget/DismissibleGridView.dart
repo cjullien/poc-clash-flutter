41c749a00ad48ef683866a0a6d8d22bf284712c8
FlutLab.io <>
2022-02-03 07:48:46 +0000

    Autosave

---
+++ lib/widget/DismissibleGridView.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'package:adress_book/models/Person.dart';
+import 'package:adress_book/widget/ContactCard.dart';
+
+class DismissibleGridView extends StatefulWidget {
+  final List<Person> persons;
+
+  DismissibleGridView(this.persons) {}
+
+  @override
+  _DismissibleGridView createState() => _DismissibleGridView();
+}
+
+class _DismissibleGridView extends State<DismissibleGridView> {
+  Widget build(BuildContext context) {
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: (MediaQuery.of(context).size.width ~/ 300),
+      children: widget.persons
+          .map(
+            (p) => Dismissible(
+              key: UniqueKey(),
+              child: ContactCard(p),
+              // Provide a function that tells the app
+              // what to do after an item has been swiped away.
+              confirmDismiss: (direction) async {
+                if (direction == DismissDirection.startToEnd) {
+                  /// edit item
+                  // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('${person.name} edit')));
+                  return false;
+                } else if (direction == DismissDirection.endToStart) {
+                  setState(() {
+                    widget.persons.remove(p);
+                  });
+
+                  /// delete
+                  //ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('${person.name} dismissed')));
+                  return true;
+                }
+              },
+              // Show a red background as the item is swiped away.
+              background: Container(
+                alignment: Alignment.centerLeft,
+                child: Icon(
+                  Icons.edit,
+                  size: 40,
+                ),
+              ),
+              secondaryBackground: Container(
+                alignment: Alignment.centerRight,
+                child: Icon(
+                  Icons.delete,
+                  size: 40,
+                ),
+              ),
+            ),
+          )
+          .toList(),
+    );
+  }
+}
--- lib/widget/FutureListPersonBuilderWidget.dart
+++ lib/widget/FutureListPersonBuilderWidget.dart
@@ -3,72 +3,21 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 import 'package:adress_book/models/Person.dart';
-import 'package:adress_book/widget/ContactCard.dart';
+import 'package:adress_book/widget/DismissibleGridView.dart';
 
-class FutureListPersonBuilderWidget extends StatefulWidget {
+class FutureListPersonBuilderWidget extends StatelessWidget {
   final List<Person> persons;
 
   late final Future<dynamic> futureDynamic;
 
   FutureListPersonBuilderWidget(this.persons, this.futureDynamic) {}
 
-  @override
-  _FutureListPersonBuilderWidget createState() => _FutureListPersonBuilderWidget();
-}
-
-class _FutureListPersonBuilderWidget extends State<FutureListPersonBuilderWidget> {
   Widget build(BuildContext context) {
     return FutureBuilder<dynamic>(
-      future: widget.futureDynamic,
+      future: futureDynamic,
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          return GridView.count(
-            primary: false,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: (MediaQuery.of(context).size.width ~/ 300),
-            children: widget.persons
-                .map(
-                  (p) => Dismissible(
-                    key: UniqueKey(),
-                    child: ContactCard(p),
-                    // Provide a function that tells the app
-                    // what to do after an item has been swiped away.
-                    confirmDismiss: (direction) async {
-                      if (direction == DismissDirection.startToEnd) {
-                        /// edit item
-                        // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('${person.name} edit')));
-                        return false;
-                      } else if (direction == DismissDirection.endToStart) {
-                        setState(() {
-                          widget.persons.remove(p);
-                        });
-
-                        /// delete
-                        //ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('${person.name} dismissed')));
-                        return true;
-                      }
-                    },
-                    // Show a red background as the item is swiped away.
-                    background: Container(
-                      alignment: Alignment.centerLeft,
-                      child: Icon(
-                        Icons.edit,
-                        size: 40,
-                      ),
-                    ),
-                    secondaryBackground: Container(
-                      alignment: Alignment.centerRight,
-                      child: Icon(
-                        Icons.delete,
-                        size: 40,
-                      ),
-                    ),
-                  ),
-                )
-                .toList(),
-          );
+          return DismissibleGridView(persons);
         } else {
           return Center(
             child: CircularProgressIndicator(
