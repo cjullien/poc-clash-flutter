9c55899f1c4de9ae49618a250d0d4f5a10a6d753
FlutLab.io <>
2022-02-03 21:15:59 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -46,37 +46,43 @@ class _MyAppState extends State<MyApp> {
       title: _constantes.App.title,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('AppBar Demo'),
-          actions: <Widget>[
+          //leading: Icon(Icons.menu),
+          title: Text(_constantes.App.title),
+          actions: [
             IconButton(
-              icon: const Icon(Icons.add_alert),
-              tooltip: 'Show Snackbar',
+              icon: Icon(Icons.person_add_alt),
               onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('This is a snackbar')));
+                final snackBar = SnackBar(
+                  behavior: SnackBarBehavior.floating,
+                  content: Text('TODO Ajouter une personne'),
+                  /*action: SnackBarAction(
+                      label: 'Action',
+                      onPressed: () {},
+                    ),*/
+                );
+                Scaffold.of(context).showSnackBar(snackBar);
               },
             ),
-            IconButton(
-              icon: const Icon(Icons.navigate_next),
-              tooltip: 'Go to the next page',
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute<void>(
-                  builder: (BuildContext context) {
-                    return Scaffold(
-                      appBar: AppBar(
-                        title: const Text('Next page'),
-                      ),
-                      body: const Center(
-                        child: Text(
-                          'This is the next page',
-                          style: TextStyle(fontSize: 24),
-                        ),
-                      ),
-                    );
-                  },
-                ));
-              },
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  final snackBar = SnackBar(
+                    behavior: SnackBarBehavior.floating,
+                    content: Text('TODO Ajouter une personne'),
+                    /*action: SnackBarAction(
+                      label: 'Action',
+                      onPressed: () {},
+                    ),*/
+                  );
+                  Scaffold.of(context).showSnackBar(snackBar);
+                },
+              ),
             ),
+            Icon(Icons.more_vert),
           ],
+          backgroundColor: Colors.grey,
         ),
         body: FutureListPersonBuilderWidget(persons, futureDynamic),
         drawer: Drawer(
