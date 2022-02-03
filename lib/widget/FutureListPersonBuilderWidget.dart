8bb5cf0fd6d7c28ff9eb807ec2be1b7832eefb51
FlutLab.io <>
2022-02-03 07:37:49 +0000

    Autosave

--- lib/widget/FutureListPersonBuilderWidget.dart
+++ lib/widget/FutureListPersonBuilderWidget.dart
@@ -59,7 +59,7 @@ class _FutureListPersonBuilderWidget extends State<FutureListPersonBuilderWidget
                       alignment: Alignment.centerRight,
                       child: Icon(
                         Icons.delete,
-                        size: 50,
+                        size: 40,
                       ),
                     ),
                   ),
