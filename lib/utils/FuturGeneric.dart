7085b9f5bd1088e636492916e142e32f1de365a3
FlutLab.io <>
2022-02-06 23:23:23 +0000

    Autosave

--- lib/utils/FuturGeneric.dart
+++ lib/utils/FuturGeneric.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:adress_book/utils/Constantes.dart' as _constantes;
 
 Future<dynamic> fetchDynamic() async {
-  final response = await http.get(Uri.parse(_constantes.Url.url2));
+  final response = await http.get(Uri.parse(_constantes.Url.url2 + '/1'));
 
   if (response.statusCode == 200) {
     return jsonDecode(response.body);
