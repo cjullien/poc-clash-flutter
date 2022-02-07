c9819bcf7bf03380fe0a32931f410668b121346c
FlutLab.io <>
2022-02-07 01:13:49 +0000

    Autosave

--- lib/utils/FuturGeneric.dart
+++ lib/utils/FuturGeneric.dart
@@ -6,7 +6,7 @@ import 'package:adress_book/utils/Constantes.dart' as _constantes;
 Future<dynamic> fetchDynamic() async {
   final response = await http.get(Uri.parse(_constantes.Url.url2));
   if (response.statusCode == 200) {
-    return jsonDecode(response.body);
+    return jsonDecode(response.body)['_embedded']['users'][0];
   } else {
     throw Exception('Failed to load dynamic');
   }
