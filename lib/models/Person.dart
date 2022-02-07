131c2264c7fe20bf3e5983adfe34c4a10667eda9
FlutLab.io <>
2022-02-07 00:30:11 +0000

    Autosave

--- lib/models/Person.dart
+++ lib/models/Person.dart
@@ -1,4 +1,4 @@
-class Person {
+/*class Person {
   Person({this.firstname = "", this.name = "", this.email = "", this.phone = "", this.picture = ""});
 
   final String firstname;
@@ -25,5 +25,75 @@ class Person {
         'email': instance.email,
         'phone': instance.phone,
         'picture': instance.picture,
+      };
+}
+*/
+// To parse this JSON data, do
+//
+//     final usershal = usershalFromJson(jsonString);
+
+import 'dart:convert';
+
+Usershal usershalFromJson(String str) => Usershal.fromJson(json.decode(str));
+
+String usershalToJson(Usershal data) => json.encode(data.toJson());
+
+class Usershal {
+  Usershal({
+    this.embedded,
+  });
+
+  Embedded embedded;
+
+  factory Usershal.fromJson(Map<String, dynamic> json) => Usershal(
+        embedded: Embedded.fromJson(json["_embedded"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "_embedded": embedded.toJson(),
+      };
+}
+
+class Embedded {
+  Embedded({
+    this.users,
+  });
+
+  List<User> users;
+
+  factory Embedded.fromJson(Map<String, dynamic> json) => Embedded(
+        users: List<User>.from(json["users"].map((x) => User.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "users": List<dynamic>.from(users.map((x) => x.toJson())),
+      };
+}
+
+class User {
+  User({
+    this.username = "",
+    this.name = "",
+    this.email = "",
+    this.phone = "",
+  });
+
+  String username;
+  String name;
+  String email;
+  String phone;
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        username: json["username"],
+        name: json["name"],
+        email: json["email"],
+        phone: json["phone"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        "name": name,
+        "email": email,
+        "phone": phone,
       };
 }
