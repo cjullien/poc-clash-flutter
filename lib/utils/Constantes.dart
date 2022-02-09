67fec2dd3689135e4456c8db7278ab1dae9d85e5
FlutLab.io <>
2022-02-09 21:37:56 +0000

    Autosave

--- lib/utils/Constantes.dart
+++ lib/utils/Constantes.dart
@@ -7,6 +7,5 @@ class App {
 }
 
 class Url {
-  static const url1 = 'https://jsonplaceholder.typicode.com/users/';
-  static const url2 = 'https://8080-cjullien-pocclashcodevsl-fd6igzeth8k.ws-eu30.gitpod.io/api/users';
+  static const url = 'https://8080-cjullien-pocclashcodevsl-fd6igzeth8k.ws-eu30.gitpod.io/api/users';
 }
